089344931cd8f126f5840d7973517cc8c08dab82
Matt Bovel <matthieu@bovel.net>
2021-12-17 04:26:01 +0100

    Add binary trees test

--- src/it/scala/inox/solvers/OptimizerSuite.scala
+++ src/it/scala/inox/solvers/OptimizerSuite.scala
@@ -10,7 +10,7 @@ class OptimizerSuite extends SolvingTestSuite with DatastructureUtils {
   import SolverResponses._
 
   override def configurations =
-    for (nme <- Seq("nativez3-opt", "smt-z3-opt")) yield {
+    for (nme <- Seq(/*"nativez3-opt",*/ "smt-z3-opt")) yield {
       Seq(optSelectedSolvers(Set(nme)))
     }
 
@@ -18,7 +18,66 @@ class OptimizerSuite extends SolvingTestSuite with DatastructureUtils {
     "solvr=" + options.findOptionOrDefault(optSelectedSolvers).head
   }
 
-  val program = inox.Program(inox.trees)(NoSymbols)
+  val treeId: Identifier = FreshIdentifier("Tree")
+  val nodeId: Identifier = FreshIdentifier("Node")
+  val emptyId: Identifier = FreshIdentifier("Empty")
+
+  val valueId: Identifier = FreshIdentifier("value")
+  val leftId: Identifier = FreshIdentifier("left")
+  val rightId: Identifier = FreshIdentifier("right")
+
+  val treeSort = mkSort(treeId)("A") { case Seq(tp) =>
+    Seq(
+      (nodeId, Seq(ValDef(valueId, IntegerType()), ValDef(leftId, T(treeId)(tp)), ValDef(rightId, T(treeId)(tp)))),
+      (emptyId, Seq())
+    )
+  }
+
+  val sizeId: Identifier = FreshIdentifier("size")
+
+  val sizeFunction = mkFunDef(sizeId)("A") { case Seq(tp) => (
+    Seq("t" :: T(treeId)(tp)),
+    IntegerType(),
+    { case Seq(t) =>
+        let("res" :: IntegerType(),
+        if_ (t is nodeId) {
+          E(BigInt(1)) + E(sizeId)(tp)(t.getField(leftId)) + E(sizeId)(tp)(t.getField(rightId))
+        } else_ {
+          E(BigInt(0))
+        }
+      ) (res => Assume(res >= E(BigInt(0)), res))
+    }
+  )}
+
+  val heightId: Identifier = FreshIdentifier("height")
+
+  val heightFunction = mkFunDef(heightId)("A") { case Seq(tp) => (
+    Seq("t" :: T(treeId)(tp)),
+    IntegerType(),
+    { case Seq(t) =>
+      if_ (t is nodeId) {
+        E(BigInt(1)) +
+        let("res" :: IntegerType(),
+          let("leftHeight" :: IntegerType(), E(heightId)(tp)(t.getField(leftId))) (leftHeight => 
+            let("rightHeight" :: IntegerType(), E(heightId)(tp)(t.getField(rightId))) (rightHeight => 
+              if_ (leftHeight >= rightHeight) { leftHeight }
+              else_ { rightHeight  }
+            )
+          )
+        ) (res => Assume(res >= E(BigInt(0)), res))
+      } else_ {
+        E(BigInt(0))
+      }
+    }
+  )}
+
+  implicit val symbols: inox.trees.Symbols = {
+    NoSymbols
+      .withFunctions(Seq(sizeFunction, heightFunction))
+      .withSorts(Seq(treeSort))
+  }
+
+  val program = inox.Program(inox.trees)(symbols)
 
   def getIntegerValue(model: program.Model, v: Variable): BigInt = model.vars.get(v.toVal).get match {
     case IntegerLiteral(n) => n
@@ -45,19 +104,18 @@ class OptimizerSuite extends SolvingTestSuite with DatastructureUtils {
     }
   }
 
-  test("one satisfiable soft constraint out of two") { implicit ctx =>
-    val v = Variable.fresh("x", IntegerType())
-    val softClause1 = GreaterThan(v, IntegerLiteral(BigInt(10)))
-    val softClause2 = GreaterThan(IntegerLiteral(BigInt(-10)), v)
+  test("binary trees") { implicit ctx =>
+    val aTree = Variable.fresh("aTree", T(treeId)(IntegerType()))
+    val clause = Equals(E(heightId)(IntegerType())(aTree), IntegerLiteral(BigInt(3)))
 
     val factory = SolverFactory.optimizer(program, ctx)
     val optimizer = factory.getNewSolver()
     try {
-      optimizer.assertCnstr(softClause1, 1)
-      optimizer.assertCnstr(softClause2, 2)
+      optimizer.assertCnstr(clause)
+      optimizer.minimize(E(sizeId)(IntegerType())(aTree))
       optimizer.check(Model) match {
         case SatWithModel(model) =>
-          assert(getIntegerValue(model, v) < -10)
+          println(model)
         case _ =>
           fail("Expected sat-with-model")
       }
